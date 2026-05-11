a1bf464e7d0b0cfd436a2c52e79e756f534d0eee
montedopasto <mdopasto@gmail.com>
2026-05-11 14:24:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
+const msalConfig = {
+    auth: {
+        clientId: CONFIG.clientId,
+        authority: CONFIG.authority,
+        redirectUri: CONFIG.redirectUri
+    }
+};
 
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+const loginBtn = document.getElementById("loginBtn");
+const statusDiv = document.getElementById("status");
+
+loginBtn.addEventListener("click", loginMicrosoft);
+
+async function loginMicrosoft(){
+
+    try{
+
+        statusDiv.innerHTML = "A iniciar sessão...";
+
+        const loginResponse = await msalInstance.loginPopup({
+            scopes: CONFIG.scopes
+        });
+
+        console.log(loginResponse);
+
+        statusDiv.innerHTML = "Login efetuado com sucesso.";
+
+        localStorage.setItem(
+            "accessToken",
+            loginResponse.accessToken
+        );
+
+        localStorage.setItem(
+            "userName",
+            loginResponse.account.name
+        );
+
+        localStorage.setItem(
+            "userEmail",
+            loginResponse.account.username
+        );
+
+        setTimeout(() => {
+
+            window.location.href = "dashboard.html";
+
+        },1000);
+
+    }
+    catch(error){
+
+        console.error(error);
+
+        statusDiv.innerHTML = "Erro no login.";
+
+    }
+
+}
