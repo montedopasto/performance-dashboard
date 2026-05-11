0d41df7f61d101a1ce24d2c754e30bb818986fce
montedopasto <mdopasto@gmail.com>
2026-05-11 14:22:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,28 @@
+const CONFIG = {
 
+    clientId: "247b5853-5e9d-4907-8955-a1fa67adb33a",
+
+    tenantId: "COLOCA_AQUI_TENANT_ID",
+
+    redirectUri: window.location.origin + "/performance-dashboard/",
+
+    authority: "https://login.microsoftonline.com/COLOCA_AQUI_TENANT_ID",
+
+    scopes: [
+        "User.Read",
+        "Sites.ReadWrite.All",
+        "Files.ReadWrite.All"
+    ],
+
+    siteName: "App Performance Dashboard",
+
+    lists: {
+        utilizadores: "UtilizadoresDashboard",
+        kpis: "KPIsDashboard",
+        funcoes: "FuncoesDashboard",
+        historico: "HistoricoKPIs",
+        softskills: "SoftSkillsDashboard",
+        configuracao: "ConfiguracaoFuncoes"
+    }
+
+};
